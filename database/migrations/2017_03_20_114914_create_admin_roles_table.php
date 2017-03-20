9dbf5e6e0423122a94bacc2f65626266908424f1
yangchunrun <263270148@qq.com>
2017-03-20 11:50:16 +0800

    data fix

--- database/migrations/2017_03_20_114914_create_admin_roles_table.php
+++ database/migrations/2017_03_20_114914_create_admin_roles_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateRolesTable extends Migration {
+class CreateAdminRolesTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,12 +12,12 @@ class CreateRolesTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('roles', function(Blueprint $table)
+		Schema::create('admin_roles', function(Blueprint $table)
 		{
 			$table->increments('id');
-			$table->string('name')->unique();
-			$table->string('display_name')->nullable();
-			$table->string('description')->nullable();
+			$table->string('name', 50)->unique('roles_name_unique');
+			$table->string('display_name', 50)->nullable();
+			$table->string('description', 100)->nullable();
 			$table->timestamps();
 		});
 	}
@@ -30,7 +30,7 @@ class CreateRolesTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('roles');
+		Schema::drop('admin_roles');
 	}
 
 }
